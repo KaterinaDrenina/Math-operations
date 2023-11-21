4368df2d745c52101abc06469af67c5e217c1119
Katerina <edrenina2@gmail.com>
2023-11-21 13:33:11 +0200

    .

---
+++ app.js
@@ -0,0 +1,34 @@
+let x = prompt ("Enter first number");
+let y = prompt ("Enter second number");
+
+let addition = +x + +y;
+let subtraction = x - y;
+let multiplication = x * y;
+let division = x / y;
+
+let result = 
+x +
+"+" +
+y + 
+"=" +
+addition +
+"\n" +
+x +
+"-" +
+y + 
+"=" +
+subtraction +
+"\n" +
+x +
+"*" +
+y + 
+"=" +
+multiplication +
+"\n" +
+x +
+"/" +
+y + 
+"=" +
+division;
+
+alert (result);
